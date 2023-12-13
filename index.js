3683bdf6ac8e123ef077b5c07c275d60fcc4b672
brandn3 <brandon.lanoue03@gmail.com>
2023-12-12 19:12:54 -0500

    ver 0.0.2

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ let Main = document.createElement('div');
 let Tabs = document.createElement('div');
 let ActionDiv = document.createElement('div');
 let MortalDiv = document.createElement('div');
+let ToolTipDiv = document.createElement('div')
+let LogDiv = document.createElement('div');
 
 let Mortals = {};
-
+let Actions = {};
 
 
 
@@ -16,10 +18,15 @@ function setUp()
     Tabs.setAttribute("id", "Tabs");
     ActionDiv.setAttribute("id", "ActionDiv");
     MortalDiv.setAttribute("id", "MortalDiv");
+    ToolTipDiv.setAttribute("id", "ToolTipDiv");
+    LogDiv.setAttribute("id", "LogDiv");
 
     Mortals["john"] = new Mortal;
     Mortals["john1"] = new Mortal;
 
+    Actions["test"] = new Action;
+    Actions["test2"] = new Action;
+    Actions["test3"] = new Action;
 
 
     document.body.appendChild(Main)
@@ -27,6 +34,8 @@ function setUp()
     Main.appendChild(Tabs)
     Main.appendChild(ActionDiv)
     Main.appendChild(MortalDiv)
+    Main.appendChild(ToolTipDiv);
+    Main.appendChild(LogDiv)
 }
 
 
@@ -37,6 +46,7 @@ class Mortal
     StatDiv = document.createElement("div");
     SkillDiv = document.createElement("div");
     StaminaDiv = document.createElement("div");
+    StaminaBarDiv = document.createElement("div");
 
     constructor()
     {
@@ -46,19 +56,45 @@ class Mortal
 
     CreateHtml()
     {
-        this.MainDiv.classList.add("MainDiv");
-        this.NameDiv.classList.add("NameDiv");
-        this.StatDiv.classList.add("StatDiv");
-        this.SkillDiv.classList.add("SkillDiv");
-        this.StaminaDiv.classList.add("StaminaDiv");
+        this.MainDiv.classList.add("MortalMainDiv");
+        this.NameDiv.classList.add("MortalNameDiv");
+        this.StatDiv.classList.add("MortalStatDiv");
+        this.SkillDiv.classList.add("MortalSkillDiv");
+        this.StaminaDiv.classList.add("MortalStaminaDiv");
+        this.StaminaBarDiv.classList.add("MortalStaminaBarDiv")
+        
+
+        this.NameDiv.innerText = "John";
+        this.StatDiv.innerText = "Stat"
+        this.SkillDiv.innerText = "Skill"
+        this.StaminaDiv.innerText = "100/100"
+
+        this.StaminaDiv.appendChild(this.StaminaBarDiv)
 
         this.MainDiv.appendChild(this.NameDiv);
         this.MainDiv.appendChild(this.StatDiv);
         this.MainDiv.appendChild(this.SkillDiv);
+        this.MainDiv.appendChild(this.StaminaDiv)
+        
 
         MortalDiv.appendChild(this.MainDiv)
     }
 }
+class Action
+{
+    MainDiv = document.createElement('div')
+    constructor()
+    {
 
+        this.CreateHtml();
+    }
+    CreateHtml()
+    {
+
+        this.MainDiv.classList.add("ActionMainDiv");
+
+        ActionDiv.appendChild(this.MainDiv);
+    }
+}
 
 setUp()
